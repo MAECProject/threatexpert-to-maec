9b888785ee200f7fcf1ac8af3bd9f8ac30923fa7
apsillers <asillers@mitre.org>
2014-10-14 11:29:02 -0400

    Add ScriptOptions and refactor input argument parsing

--- threatexpert_to_maec.py
+++ threatexpert_to_maec.py
@@ -22,6 +22,7 @@ import sys
 import os
 import traceback
 from maec.misc.options import ScriptOptions
+import argparse
 
 #Create a MAEC output file from a ThreatExpert input file
 def create_maec(inputfile, outpath, verbose_error_mode, options):
@@ -38,8 +39,8 @@ def create_maec(inputfile, outpath, verbose_error_mode, options):
             print "Wrote to " + outpath
             
         except Exception, err:
-           print('\nError: %s\n' % str(err))
-           if verbose_error_mode:
+            print('\nError: %s\n' % str(err))
+            if verbose_error_mode:
                 traceback.print_exc()
     else:
         print('\nError: Input file not found or inaccessible.')
@@ -52,55 +53,57 @@ def usage():
     
 USAGE_TEXT = """
 ThreatExpert XML Output --> MAEC XML Converter Utility
-v0.95 BETA // Supports MAEC v4.1 and CybOX v2.1
+v0.96 BETA // Supports MAEC v4.1 and CybOX v2.1
 
-Usage: python threatexpert_to_maec.py <special arguments> -i <input threatexpert xml output> -o <output maec xml file> 
-       OR python threatexpert_to_maec.py <special arguments> -d <directory>
+Usage: python threatexpert_to_maec.py <input threatexpert xml output> <output maec xml file> <flags>
 
-Special arguments are as follows (all are optional):
--v : verbose error mode (prints tracebacks of any errors during execution).
+Flags:
+--deduplicate, -dd: flag to deduplicate objects in MAEC output
+--dereference, -dr: flag to dereference the MAEC output
+--normalize, -n: flag to normalize the MAEC output
+--verbose, -v: print verbose error output (tracebacks)
 
 """    
 def main():
-    options = ScriptOptions()
-    verbose_error_mode = 0
-    infilename = ''
-    outfilename = ''
-    directoryname = ''
-    
-    #Get the command-line arguments
-    args = sys.argv[1:]
-    
-    if len(args) < 2:
-        usage()
-        sys.exit(1)
-        
-    for i in range(0,len(args)):
-        if args[i] == '-v':
-            verbose_error_mode = 1
-        elif args[i] == '-i':
-            infilename = args[i+1]
-        elif args[i] == '-o':
-            outfilename = args[i+1]
-        elif args[i] == '-d':
-            directoryname = args[i+1]
-        elif args[i] == '--dedup':
-            options.deduplicate_bundles = True
-        elif args[i] == '--deref':
-            options.dereference_bundles = True
-        elif args[i] == '--normalize':
-            options.normalize_bundles = True
 
-    if directoryname != '':
-        for filename in os.listdir(directoryname):
-            if '.xml' not in filename:
-                pass
-            else:
-                outfilename = filename.rstrip('.xml') + '_maec.xml'
-                create_maec(os.path.join(directoryname, filename), outfilename, verbose_error_mode, options)
-    #Basic input file checking
-    elif infilename != '' and outfilename != '':
-        create_maec(infilename, outfilename, verbose_error_mode, options)
+    outfilename = ''
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="ThreatExpert to MAEC Translator")
+    parser.add_argument("input", help="the name of the input ThreatExpert XML file OR directory of files to translate to MAEC")
+    parser.add_argument("output", help="the name of the MAEC XML file OR directory to which the output will be written")
+    parser.add_argument("--verbose", "-v", help="enable verbose error output mode", action="store_true", default=False)
+    parser.add_argument("--deduplicate", "-dd", help="deduplicate the MAEC output (Objects only)", action="store_true", default=False)
+    parser.add_argument("--normalize", "-n", help="normalize the MAEC output (Objects only)", action="store_true", default=False)
+    parser.add_argument("--dereference", "-dr", help="dereference the MAEC output (Objects only)", action="store_true", default=False)
+    args = parser.parse_args()
+    
+    # Build up the options instance based on the command-line input
+    options = ScriptOptions()
+    options.deduplicate_bundles = args.deduplicate
+    options.normalize_bundles = args.normalize
+    options.dereference_bundles = args.dereference
+
+
+    # Test if the input is a directory or file
+    if os.path.isfile(args.input):
+        outfilename = args.output
+        # Test if the output is a directory
+        # If so, concatenate "_maec.xml" to the input filename
+        # and use this as the output filename
+        if os.path.isdir(args.output):
+            outfilename = os.path.join(args.output, str(os.path.basename(args.input))[:-4] + "_maec.xml")
+        # If we're dealing with a single file, just call create_maec()
+        create_maec(args.input, outfilename, args.verbose, options)
+    # If a directory was specified, perform the corresponding conversion
+    elif os.path.isdir(args.input):
+        # Iterate and try to parse/convert each file in the directory
+        for filename in os.listdir(args.input):
+            # Only handle XML files
+            if str(filename)[-3:] != "xml":
+                print str("Error: {0} does not appear to be an XML file. Skipping.\n").format(filename)
+                continue
+            outfilename = str(filename)[:-4] + "_maec.xml"
+            create_maec(os.path.join(args.input, filename), os.path.join(args.output, outfilename), args.verbose, options)
         
 if __name__ == "__main__":
     main()    
